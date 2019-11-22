72eedc7cfea484b09a39b72f1e34b4d151bcba82
Salifu Sani Rich <iamsarscode@gmail.com>
2019-11-22 14:28:42 +0100

    fix(typo): remove redundant text

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -16,13 +16,6 @@ function Intro() {
         Enthusiast based in Lagos, Nigeria. <br /> I design user interfaces and
         write code that works on the web.
       </p>
-      <p>
-        This is the first version of my personal site and it's opensource, you
-        can view the{" "}
-        <a href="https://github.com/sarscode/sarscode.dev">
-          source code on github.
-        </a>
-      </p>
       <Social />
     </main>
   );
